3aa0e0b1101fc9c37dc1492e2ebd16e5f89dca23
Alexander <aleksand-isaev@yandex.ru>
2018-02-09 17:21:48 +0400

    api host was changed

--- src/components/auth/SignUpForm/index.js
+++ src/components/auth/SignUpForm/index.js
@@ -107,7 +107,7 @@ class SignUpForm extends Component {
             <Field
               component={RenderCheckbox}
               label={<span>
-                I agree with <a href="https://jincor.com/en/agreement" target="_blank">Terms of Services</a>
+                I agree with <a href="#" target="_blank">Terms of Services</a>
               </span>}
               name="agreeTos"
               validate={required}/>
